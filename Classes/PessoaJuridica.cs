b12747459c138ca7f46285f430aa65900166313d
danieldmonteiro <danieldmonteiro@gmail.com>
2022-07-05 22:49:35 -0300

    Encontro Remoto 4

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using senai_uc12.Interfaces;
 
 namespace senai_uc12.Classes
@@ -17,10 +18,35 @@ namespace senai_uc12.Classes
         {
             throw new NotImplementedException();
         }
-
+        //xx.xxx.xxx/0001-xx --------- xxxxxxxx0001xx
         public bool ValidarCnpj(string cnpj)
         {
-            throw new NotImplementedException();
+            bool retornoCnpjvalido = Regex.IsMatch(cnpj, @"(⌃(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)");
+        
+        if (retornoCnpjvalido == true)
+        {
+            if (cnpj.Length == 18)
+            {
+
+                string substringCnpj = cnpj.Substring(11, 4);
+
+                if (substringCnpj == "0001")
+                {
+                    return true;
+                }
+
+            }else if (cnpj.Length == 14)
+            {
+                string substringCnpj = cnpj.Substring(8, 4);
+
+                if (substringCnpj == "0001")
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+        
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
 ﻿using senai_uc12.Classes;
 
-PessoaFisica novaPf = new PessoaFisica();
-PessoaFisica metodosPf = new PessoaFisica();
-Endereco novoEndPf = new Endereco();
+//PessoaFisica novaPf = new PessoaFisica();
+//PessoaFisica metodosPf = new PessoaFisica();
+//Endereco novoEndPf = new Endereco();
 
-novaPf.Nome = "Daniel";
-novaPf.dataNasc = new DateTime(2000 , 01 , 01);
-novaPf.Cpf = "08680901741";
-novaPf.Rendimento = 3500.5f;
+//novaPf.Nome = "Daniel";
+//novaPf.dataNasc = new DateTime(2000 , 01 , 01);
+//novaPf.Cpf = "08680901741";
+//novaPf.Rendimento = 3500.5f;
 
-novoEndPf.logradouro = "Alameda Barão de Limeira";
-novoEndPf.numero = 539;
-novoEndPf.complemento = "SENAI Informática";
-novoEndPf.endComercial = true;
+//novoEndPf.logradouro = "Alameda Barão de Limeira";
+//novoEndPf.numero = 539;
+//novoEndPf.complemento = "SENAI Informática";
+//novoEndPf.endComercial = true;
 
-novaPf.Endereco = novoEndPf;
+//novaPf.Endereco = novoEndPf;
 
-Console.WriteLine(@$"
-Nome: {novaPf.Nome}
-Endereço: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
-Maior de Idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
+//Console.WriteLine(@$"
+//Nome: {novaPf.Nome}
+//Endereço: {novaPf.Endereco.logradouro}, {novaPf.Endereco.numero}
+//Maior de Idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
 
-");
+//");
 
 //Console.WriteLine(metodosPf.ValidarDataNasc("01/01/2000"));
 
@@ -32,4 +32,28 @@ Maior de Idade: {metodosPf.ValidarDataNasc(novaPf.dataNasc)}
 //Console.WriteLine($"Nome: {novaPf.Nome}");
 //Console.WriteLine("Nome: " + novaPf.Nome);
 
+PessoaJuridica novaPj = new PessoaJuridica();
+PessoaJuridica metodosPj = new PessoaJuridica();
+Endereco novoEndPj = new Endereco();
 
+novaPj.Nome = "Nome Pj";
+novaPj.RazaoSocial = "Razão Social Pj";
+novaPj.Cnpj = "00000000000100";
+novaPj.Rendimento = 10000.5f;
+
+novoEndPj.logradouro = "Alameda Barão de Limeira";
+novoEndPj.numero = 539;
+novoEndPj.complemento = "SENAI Informática";
+novoEndPj.endComercial = true;
+
+novaPj.Endereco = novoEndPj;
+
+//Console.WriteLine(metodosPj.ValidarCnpj(novaPj.Cnpj));
+
+Console.WriteLine(@$"
+Nome: {novaPj.Nome}
+Razão Social: {novaPj.RazaoSocial}
+CNPJ: {novaPj.Cnpj}, Válido: {metodosPj.ValidarCnpj(novaPj.Cnpj)}
+Endereço: {novaPj.Endereco.logradouro}, Número: {novaPj.Endereco.numero}
+Complemento: {novaPj.Endereco.complemento}
+");
